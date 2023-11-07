8485d5d2f10d35979984741567f3a3e7bf6cefd6
Nίκος Μαυραπίδης <nikosm9999@gmail.com>
2023-11-07 17:11:58 +0200

    cv addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -141,3 +141,30 @@ for (let i = 0; i < navigationLinks.length; i++) {
     }
   });
 }
+
+//CV
+// Get the modal
+var modal = document.getElementById("cvModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("cvLink");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.classList.add("show");
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.classList.remove("show");
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.classList.remove("show");
+  }
+};
